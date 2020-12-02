ea29f565020126cdcb5f2b9694916e40241c94e9
Kristine Du <kristine.a.du@gmail.com>
2020-12-02 11:07:42 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+
+    render json: @user
+  end
+
+  def create
+    @user = User.create(
+      name: params[:name],
+      email: params[:email],
+      password: params[:password]
+    )
+  end
+end
